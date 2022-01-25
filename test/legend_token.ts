5460b94e2276e26d31db733820cd2c3e64c734c1
0xfrosty <95886535+0xfrosty@users.noreply.github.com>
2022-01-25 09:30:56 +0100

    BC-3 Contract for LEGEND token (#1)
    * Define useful yarn scripts
    * Install openzeppelin
    * Implement ERC20 LEGEND token
    * Fix Hardhat issue with Node 17
      https://github.com/webpack/webpack/issues/14532#issue-1030624134
    * Upgrade to solc 0.8.9 - latest Hardhat supported version
    * Add yarn script to compile project
    * Miscellaneous updates to package.json
    * Increase test coverage
    * Update README.md

---
+++ test/legend_token.ts
@@ -0,0 +1,102 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+
+import { LegendToken } from "../typechain/LegendToken";
+
+/**
+ * Test LEGEND token
+ */
+describe("LegendToken", function () {
+  let legendToken: LegendToken;
+  let owner: SignerWithAddress, bob: SignerWithAddress;
+
+  const name = "Legend";
+  const symbol = "LEGEND";
+  const decimals = 18;
+  const totalTokens = "1000000000";
+  const totalSupply = ethers.utils.parseUnits(totalTokens, decimals);
+
+  /**
+   * Deploy contract before each test
+   */
+  beforeEach(async function () {
+    const LegendTokenFactory = await ethers.getContractFactory("LegendToken");
+    legendToken = (await LegendTokenFactory.deploy()) as LegendToken;
+    await legendToken.deployed();
+
+    [owner, bob] = await ethers.getSigners()
+  });
+
+  /**
+   * Check contract tracks the proper values (name, symbol, decimals, supply)
+   */
+  it(`should have the ${name} name`, async function () {
+    expect(await legendToken.name()).to.equal(name);
+  });
+
+  it(`should have the ${symbol} symbol`, async function () {
+    expect(await legendToken.symbol()).to.equal(symbol);
+  });
+
+  it(`should have ${decimals} decimals`, async function () {
+    expect(await legendToken.decimals()).to.equal(decimals);
+  });
+
+  it(`should have minted ${totalTokens} tokens`, async function () {
+    expect(await legendToken.totalSupply()).to.equal(totalSupply);
+  });
+
+  /**
+   * Check LEGEND transfers work as expected.
+   */
+  describe("Transfers", function () {
+    it("should allow owner to pause transfers", async function () {
+      expect(await legendToken.pause())
+        .to.emit(legendToken, 'Paused')
+        .withArgs(owner.address);
+
+      expect(await legendToken.paused()).to.true;
+    });
+
+    it("should allow owner to resume transfers", async function () {
+      await legendToken.pause();
+
+      expect(await legendToken.unpause())
+        .to.emit(legendToken, 'Unpaused')
+        .withArgs(owner.address);
+
+      expect(await legendToken.paused()).to.false;
+    });
+
+    it("shouldn't allow non-owner to pause transfers", async function () {
+      expect(legendToken.connect(bob).pause())                    // not awaiting here to work around
+        .to.be.revertedWith("Ownable: caller is not the owner");  // https://github.com/EthWorks/Waffle/issues/95
+
+      expect(await legendToken.paused()).to.false;
+    });
+
+    it("should allow holder to transfer tokens", async function () {
+      let amount = ethers.utils.parseUnits("100", decimals);
+      let ownerBalance = await legendToken.balanceOf(owner.address);
+      let bobBalance = await legendToken.balanceOf(bob.address);
+      let newOwnerBalance = ownerBalance.sub(amount);
+      let newBobBalance = bobBalance.add(amount);
+
+      expect(await legendToken.transfer(bob.address, amount))
+        .to.emit(legendToken, 'Transfer')
+        .withArgs(owner.address, bob.address, amount);
+
+      expect(await legendToken.balanceOf(owner.address)).to.equal(newOwnerBalance);
+      expect(await legendToken.balanceOf(bob.address)).to.equal(newBobBalance);
+    });
+
+    it("shouldn't allow holder to transfer tokens when contract is paused", async function () {
+      let amount = ethers.utils.parseUnits("100", decimals);
+
+      await legendToken.pause();
+      expect(legendToken.transfer(bob.address, amount))  // not awaiting here to work around
+        .to.be.revertedWith("Pausable: paused");         // https://github.com/EthWorks/Waffle/issues/95
+    });
+  });
+});
