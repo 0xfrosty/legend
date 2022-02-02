9c4371e8ec327f5e6059bed54a9084040e663b32
0xfrosty <0xfrosty@protonmail.com>
2022-02-02 15:04:15 +0100

    Documentation and minor refactors for vesting schedules

--- contracts/IVestingSchedule.sol
+++ contracts/IVestingSchedule.sol
@@ -1,8 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+/**
+ * @title IVestingSchedule
+ * @dev Interface for contracts that define the duration of vesting periods according to different schedules. E.g.
+ * a schedule might refer to a seed round, while other could refer to a public sale - and the vesting duration could
+ * be different in each case.
+ */
 interface IVestingSchedule {
-    error InvalidSchedule(uint8 given, uint8 min, uint8 max);
-
-    function getDuration(uint8 scheduleId) external pure returns (uint64);
+    /**
+     * @dev Get the duration of a given vesting schedule
+     * @param scheduleId Id of the schedule
+     */
+    function getDuration(uint8 scheduleId) external view returns (uint64);
 }
--- contracts/LegendVestingSchedule.sol
+++ contracts/LegendVestingSchedule.sol
@@ -3,7 +3,22 @@ pragma solidity ^0.8.9;
 
 import "./IVestingSchedule.sol";
 
+/**
+ * @title LegendVestingSchedule
+ * @dev Implementation of the {IVestingSchedule} interface for the LEGEND tokenomics
+ */
 contract LegendVestingSchedule is IVestingSchedule {
+    /**
+     * @dev Thrown when the given schedule id is not within the range of valid identifiers
+     * @param given Schedule id that triggered this error
+     * @param min Min valid schedule id
+     * @param max Max valid schedule id
+     */
+    error InvalidSchedule(uint8 given, uint8 min, uint8 max);
+
+    /**
+     * Identifiers of vesting periods according to tokenomics
+     */
     uint8 public constant SCHEDULE_SEED = 0;
     uint8 public constant SCHEDULE_STRATEGIC = 1;
     uint8 public constant SCHEDULE_PRIVATE = 2;
@@ -15,6 +30,9 @@ contract LegendVestingSchedule is IVestingSchedule {
     uint8 public constant SCHEDULE_REWARDS = 8;
     uint8 public constant SCHEDULE_RESERVE = 9;
     
+    /**
+     * Constants with the value in seconds of different vesting durations
+     */
     uint64 private constant DURATION_ZERO = 0; // no vesting
     uint64 private constant DURATION_04_MO = 10368000; // 4 * 30 * 24 * 3600 seconds
     uint64 private constant DURATION_08_MO = 20736000; // 8 * 30 * 24 * 3600 seconds
@@ -23,45 +41,44 @@ contract LegendVestingSchedule is IVestingSchedule {
     uint64 private constant DURATION_24_MO = 62208000; // 24 * 30 * 24 * 3600 seconds
     uint64 private constant DURATION_36_MO = 93312000; // 36 * 30 * 24 * 3600 seconds
 
-    function getDuration(uint8 scheduleId) public pure virtual returns (uint64) {
-        return _getDurationBySchedule(scheduleId);
-    }
-
-
-    function _getDurationBySchedule(uint8 scheduleId) internal pure returns (uint64) {
+    /**
+     * @dev Get the duration of a given vesting schedule
+     * @param scheduleId Id of the schedule
+     */
+    function getDuration(uint8 scheduleId) public pure returns (uint64) {
         if (scheduleId == SCHEDULE_SEED) {
             return DURATION_12_MO;
-        
+
         } else if (scheduleId == SCHEDULE_STRATEGIC) {
             return DURATION_10_MO;
-        
+
         } else if (scheduleId == SCHEDULE_PRIVATE) {
             return DURATION_08_MO;
-        
+
         } else if (scheduleId == SCHEDULE_PUBLIC) {
             return DURATION_04_MO;
-        
+
         } else if (scheduleId == SCHEDULE_TEAM) {
             return DURATION_24_MO;
-        
+
         } else if (scheduleId == SCHEDULE_MARKETING) {
             return DURATION_36_MO;
-        
+
         } else if (scheduleId == SCHEDULE_LIQUIDITY) {
             return DURATION_ZERO;
-        
+
         } else if (scheduleId == SCHEDULE_ECOSYSTEM) {
             return DURATION_36_MO;
-        
+
         } else if (scheduleId == SCHEDULE_REWARDS) {
             return DURATION_24_MO;
-        
+
         } else if (scheduleId == SCHEDULE_RESERVE) {
             return DURATION_24_MO;
-        
+
         } else {
             revert InvalidSchedule({given: scheduleId, min: SCHEDULE_SEED, max: SCHEDULE_RESERVE});
-        
+
         }
     }
 }
